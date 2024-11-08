b44bf52f0732731b49fa6c34ddf0c86a9e1d587d
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-08 17:05:44 -0600

    Add files via upload

---
+++ Examen_Evaluacion2_24550354/src/main/java/com/mycompany/examen_evaluacion2_24550354/Examen_Evaluacion2_24550354.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_evaluacion2_24550354;
+
+import java.util.Scanner;
+
+
+public class Examen_Evaluacion2_24550354 {
+
+    public static void main(String[] args) {
+        int resultado, piedra, papel, tijera;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Ingresa el numero 1,2 o 3 donde 1 es piedra, 2 es papel, 3 es tijera");
+        resultado = captu.nextInt();
+            for (int i = 1; i < 3; i++) {
+            }if (resultado == 1)
+                System.out.println("papel");
+            else
+           if(resultado == 2)
+               System.out.println("tijera");              
+            else
+          if(resultado ==3 )
+                Math.random();
+   
+    }
+        }
+    
+
